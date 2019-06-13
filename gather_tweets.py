2942f583394871ed33e1d348e67bbfa32ac07329
Cinkupis <d045312@gmail.com>
2019-06-13 21:24:23 +0100

    Security changes

--- gather_tweets.py
+++ gather_tweets.py
@@ -1,12 +1,8 @@
 import tweepy
+import key_secret
 
-consumer_key = 'iFi38M09e6B41RMsndwojvNLf'
-consumer_secret = 'TmtrQZK4e7FNss99uraM5CLqg96Fpvw0tQtUCDQn9nTzBqJ6um'
-access_token = '2820632585-jG2aBWsjgkY7y8Wh4PSOjTPiz1ki54qFJNl8yMc'
-access_token_secret = 'WJQpNLRorL8OpG5UCDxygFOIMasP0jH4Bif9fgFSpPzcS'
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(key_secret.consumer_key, key_secret.consumer_secret)
+auth.set_access_token(key_secret.access_token, key_secret.access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 for tweet in tweepy.Cursor(api.search, q="#Brexit", count=10, lang="en", since="2019-06-01",
